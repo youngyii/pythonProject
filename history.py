6ec21eb0fc29ddbb7fa0450fced7f40de9a102ad
youngyii <yishinyoung0708@gmail.com>
2023-12-07 12:50:10 +0900

    project

---
+++ history.py
@@ -0,0 +1,5 @@
+def History(filename):
+    file = open(filename, "r", encoding="UTF-8")
+    res = file.read()
+    print(res)
+    file.close()
---
+++ point.py
@@ -0,0 +1,5 @@
+import random
+
+def GetPoint():
+    p= [100, 200, 300, 400, 500]
+    return random.choice(p)
